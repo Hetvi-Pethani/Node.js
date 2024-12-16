f031c44f16731750edc50c64529ba6dfa1438189
Hetvi-Pethani <hetvi.pethani75@gmail.com>
2024-12-16 11:05:31 +0530

    inialcommit

---
+++ Lec-1/index.js
@@ -0,0 +1,17 @@
+console.log("Hello, World!");
+
+
+let arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+let ans = arr1.filter((val) => {
+    return val % 2 == 0
+});
+console.log(ans);
+
+
+let arr2 = [1, 2, 3, 4, 5, 6 , 7, 8, 9]
+let ans2 =  arr2.map ((val) => {
+    return val * 2
+})
+console.log(ans2);
+
+
