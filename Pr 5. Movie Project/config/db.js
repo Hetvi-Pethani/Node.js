537f23d67c050d21dbd11266ffcf242dfc0174da
Hetvi-Pethani <hetvi.pethani75@gmail.com>
2025-02-03 10:13:25 +0530

    initalcommit

--- Pr 5. Movie Project/config/db.js
+++ Pr 5. Movie Project/config/db.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 
-const connectDb = async () => {
-    try {
-        const db = await mongoose.connect(`mongodb+srv://shrawanprajapat999:Shrawan@cluster0.rprkd.mongodb.net/movie-poster-crud`, {
-          
-        });
-        console.log(`Database is successfully connected to host: ${db.connection.host}`);
-    } catch (err) {
-        console.error("Error connecting to the database:", err);
+mongoose.connect(`mongodb://localhost/movie`)
+
+const db = mongoose.connection;
+
+db.on("connected", (err) => {
+    if (err) {
+        console.log(err);
         return false;
     }
-};
+    console.log("Connected to MongoDB");
+})
 
-module.exports = connectDb;
+module.exports = db;
